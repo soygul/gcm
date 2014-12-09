505c8a8561cb8ed3f745b10809019c90599e00e2
Teoman Soygul <teo@soygul.com>
2014-12-09 15:12:12 +0100

    add note

--- http/http.go
+++ http/http.go
@@ -1,5 +1,6 @@
 // Package http provides GCM HTTP client implementation.
 // https://developer.android.com/google/gcm/http.html
+// Note: HTTP implementation is not in place at the moment as CCS implementation is preferable.
 package http
 
 // yet to be implemented..
