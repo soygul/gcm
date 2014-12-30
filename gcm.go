04159d65cb2b1a37117f1e06dfd942f1d9bcf717
Teoman Soygul <teo@soygul.com>
2014-12-30 03:22:13 +0100

    add main gcm package

---
+++ gcm.go
@@ -0,0 +1,4 @@
+// Package gcm contains functions for queueing and sending messages through GCM CCS and HTTP.
+package gcm
+
+// todo: handle ack/nack/receipts along with connection draining messages and close/reconnects (or this might be in ccs)
